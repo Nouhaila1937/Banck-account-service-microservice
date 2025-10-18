345a1963d544560ae22609a18028b73ce30cd844
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-10-19 00:45:15 +0100

    second part

---
+++ src/main/java/org/example/bankaccountservice/web/BankAccountGraphQLController.java
@@ -0,0 +1,50 @@
+package org.example.bankaccountservice.web;
+
+import org.example.bankaccountservice.Repositories.BankAccountRepository;
+import org.example.bankaccountservice.dto.BankAccountRequestDTO;
+import org.example.bankaccountservice.dto.BankAccountResponseDTO;
+import org.example.bankaccountservice.entities.BankAccount;
+import org.example.bankaccountservice.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @Autowired
+    private AccountService accountService;
+
+    @QueryMapping
+    public List<BankAccount> accountsList(){
+        return bankAccountRepository.findAll();
+    }
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id){
+        return bankAccountRepository.findById(id).orElseThrow(()->new RuntimeException(String.format("Account with id %s not found", id)));
+    }
+
+    @MutationMapping
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount){
+        return accountService.addAccount(bankAccount);
+    }
+
+    @MutationMapping
+    public BankAccountResponseDTO updateAccount(@Argument String id, @Argument BankAccountRequestDTO bankAccount){
+        return accountService.updateAccount(id,bankAccount);
+    }
+
+    @MutationMapping
+    public Boolean deleteAccount(@Argument String id){
+        bankAccountRepository.deleteById(id);
+        return true;
+    }
+
+
+
+}
